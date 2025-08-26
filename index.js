9962fa43f5244fd22d34decf0c55f6f5a85efe55
Phenrique1628 <henriquepedro02964@gmail.com>
2025-08-26 14:47:23 -0400

    delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-let somar = function(){
-    let numero1 = document.getElementById("1").value;
-    let numero2 = document.getElementById("2").value;
-   
-    let somarNumeros = numero1 + numero2
-    document.getElementById("resultado").innerHTML = somarNumeros
-}
