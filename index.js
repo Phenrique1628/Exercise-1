69a14ee724f21d66890a3d1714c1875468bf57a4
Phenrique1628 <henriquepedro02964@gmail.com>
2025-08-26 14:41:32 -0400

    Create let somarNumeros

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ let somar = function(){
     let numero1 = document.getElementById("1").value;
     let numero2 = document.getElementById("2").value;
    
-    
+    let somarNumeros = numero1 + numero2
 }
